da95cb566cb8c3bd437a7654d028e0a1eafd6bf0
amitcarpenter <amitcarpenter199@gmail.com>
2024-10-11 19:23:16 +0530

    image not set

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,30 +4,30 @@ import { AiFillYoutube } from "react-icons/ai";
 import { Row, Col, Container } from "react-bootstrap";
 import "../footer/FooterSection.css";
 import logo from '../../assets/logo-dark.png'
+
 function Footer() {
   return (
-    <>
-      <section className="footer-con">
-        <Container>
-          <Row className="footer-row">
-            <Col lg={3} md={4} className="footer-connect">
-              <div>
-                <p>info@demolink.org</p>
-              </div>
-              <div>
-                <p> +91 8447906804</p>
-              </div>
-              <div className='brand'>
-          <Link to="/">
-            <img className='brand-logo' src={logo} alt='Brand Logo' width='173' height='40' />
-          </Link>
-        </div>
-            </Col>
+    <footer className="footer-con">
+      <Container>
+        <Row className="footer-row">
+          <Col lg={3} md={4} sm={12} className="footer-connect">
+            <p>info@demolink.org</p>
+            <p> +91 8447906804</p>
+            <Link to="/">
+              <img
+                className="brand-logo"
+                src={logo}
+                alt="Brand Logo"
+                width="173"
+                height="40"
+              />
+            </Link>
+          </Col>
 
-            <Col lg={3} md={4} className="footer-links">
-              <h5>Newsletter</h5>
-              <p>Lorem ipsum dolor sit amet.</p>
-              <div className="d-flex align-items-center">
+          <Col lg={3} md={4} sm={12} className="footer-links">
+            <h5>Newsletter</h5>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <div className="d-flex align-items-center">
               <input
                 className="form-control me-2"
                 type="email"
@@ -38,33 +38,32 @@ function Footer() {
               <button type="submit" className="btn_sub">
                 Subscribe
               </button>
-              </div>
+            </div>
 
-              <div className="footer-icons">
-                <Link to="/" className="footer-icons">
-                  <FaFacebookF />
-                </Link>
-                <Link to="/" className="footer-icons">
-                  <FaInstagram />
-                </Link>
-                <Link to="/" className="footer-icons">
-                  <AiFillYoutube />
-                </Link>
-              </div>
-            </Col>
+            <div className="footer-icons">
+              <Link to="/" className="footer-icon">
+                <FaFacebookF />
+              </Link>
+              <Link to="/" className="footer-icon">
+                <FaInstagram />
+              </Link>
+              <Link to="/" className="footer-icon">
+                <AiFillYoutube />
+              </Link>
+            </div>
+          </Col>
 
-            <Col lg={3} md={4} className="footer_about">
-              <h5>About Us</h5>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-                ad nisi repudiandae nobis minima.
-              </p>
-              <p>© 2024 Oversize. All Rights Reserved.</p>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </>
+          <Col lg={3} md={4} sm={12} className="footer_about">
+            <h5>About Us</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+              ad nisi repudiandae nobis minima.
+            </p>
+            <p>© 2024 Oversize. All Rights Reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 }
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,14 +9,26 @@ import product_img from "../../assets/product_img.jpg";
 import Footer from "../footer/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComment, faFaceFrownOpen } from "@fortawesome/free-solid-svg-icons";
-import { faMinus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMinus,
+  faTags,
+  faCrown,
+  faShoppingBag,
+} from "@fortawesome/free-solid-svg-icons";
+
 import model_img_4 from "../../assets/model_img_4.png";
 import model_img_5 from "../../assets/model_img_5.jpg";
 import model_img_6 from "../../assets/model_img_6.jpg";
 import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import model_img_3 from "../../assets/model_img_3.jpg";
+import useOnScreen from "../home/Animation";
+
 const Home = () => {
+
+  const [refHeading, isHeadingVisible] = useOnScreen({ threshold: 0.1 });
+  const [refTestimonial, isTestimonialVisible] = useOnScreen({ threshold: 0.1 })
+
   return (
     <>
       <Navbar />
@@ -25,9 +37,14 @@ const Home = () => {
           <div className="home_box">
             <Row className="home_particion">
               <Col lg={6}>
-                <div className="home_heading">
+                <div  className={`home_heading ${isHeadingVisible ? "animate" : ""}`} ref={refHeading}>
                   <h1>Personalized Selection of Women’s Clothes</h1>
-                  <h5>Over 150 models of plus size</h5>
+                  <div className="icon-text-box">
+                    <span>
+                      <FontAwesomeIcon icon={faMinus} className="minus-box1" />
+                    </span>
+                    <h5>Over 150 models of plus size</h5>
+                  </div>
                   <button className="shop_now_btn">Shop Now</button>
                 </div>
               </Col>
@@ -62,11 +79,12 @@ const Home = () => {
               </Col>
 
               <Col lg={6}>
+              <div className="shopping_box_ani">
                 <div className="sec_box_2">
                   <h3>One Size Does Not Fit All Your Needs</h3>
                   <div className="icon-text-box">
                     <span>
-                      <FontAwesomeIcon icon={faMinus} className="minus-box" />
+                      <FontAwesomeIcon icon={faMinus} className="minus-box1" />
                     </span>
 
                     <p>
@@ -108,12 +126,14 @@ const Home = () => {
                     </p>
                   </div>
                 </div>
+                </div>
               </Col>
             </Row>
           </div>
         </Container>
         {/* ---------------------------------------------------------- */}
         <div className="test_bg">
+        <div ref={refTestimonial} className={isTestimonialVisible ? "animate" : ""}>
           <h3>Testimonials</h3>
           <div className="testimonials-container">
             <div className="testmonial_con">
@@ -182,9 +202,9 @@ const Home = () => {
           <div className="button-container">
             <button className="review_btn">Send your review</button>
           </div>
+          </div>
         </div>
         {/* -------------------------------------------------------------------------------------- */}
-
         <Container>
           <Row className="position-relative">
             <Col className="contact_box">
@@ -231,42 +251,44 @@ const Home = () => {
           </Row>
         </Container>
         {/* ------------------------------------------------------------- */}
-        <Row className="our_gallery_row">
-  <Col lg={8} className="our_gallery_con">
-    <h2>Our Gallery</h2>
-    <div className="icon-text-box">
-      <span>
-        <FontAwesomeIcon icon={faMinus} className="minus-box" />
-      </span>
-      <h5>
-        Here you can take a look at our latest models of plus size clothes for women designed by Oversize.
-      </h5>
-    </div>
-    <div className="button-con">
-      <button className="contact_btn">View more</button>
-    </div>
-  </Col>
-
-  <Col lg={4}>
-    <div className="image-container">
-      <div className="our_model" style={{ position: 'relative' }}>
-        <img
-          src="https://img.freepik.com/free-photo/attractive-stylish-smiling-woman-choosing-apparel-clothing-store_285396-4642.jpg"
-          alt="model_img"
-        />
-      </div>
-      <div className="our_model_1">
+        <Row>
+          {/* <div className="our_gallery_bg"> */}
+          <Col lg={8} className="our_gallery_con">
+            <h2>Our Gallery</h2>
+            <div className="icon-text-box">
+              <span>
+                <FontAwesomeIcon icon={faMinus} className="minus-box" />
+              </span>
+              <h5>
+                Here you can take a look at our latest models of plus size
+                clothes for women designed by Oversize.
+              </h5>
+            </div>
+            <div className="button-con">
+              <button className="contact_btn">View more</button>
+            </div>
+          </Col>
+          {/* </div> */}
+          <Col lg={4}>
+            <div className="image-container">
+              <div className="our_model" style={{ position: "relative" }}>
+                <img
+                  src="https://img.freepik.com/free-photo/attractive-stylish-smiling-woman-choosing-apparel-clothing-store_285396-4642.jpg"
+                  alt="model_img"
+                />
+              </div>
+               {/* <div className="our_model_1">
         <img
           src="https://media.istockphoto.com/id/854321536/photo/look-at-this-gorgeous-dress.jpg?s=612x612&w=0&k=20&c=UyxEiEddYEFQPAoopwYs-_8xJ5vp0vKE0Sl3GnrQpK8="
           alt="model_img"
         />
-      </div>
-      <div className="our_model_2">
+      </div> */}
+      {/* <div className="our_model_2">
         <img
           src="https://img.freepik.com/free-photo/fashionable-pale-brunette-long-green-dress-black-jacket-sunglasses-standing-street-during-daytime-against-wall-light-city-building_197531-24468.jpg"
           alt="model_img"
         />
-      </div>
+      </div> */}
       <div className="our_model_3">
         <img
           src="https://t3.ftcdn.net/jpg/02/25/41/88/360_F_225418881_NEspMOE8o9hSaWUKI1UJb7F7uNKlW9Wj.jpg"
@@ -279,21 +301,93 @@ const Home = () => {
           alt="model_img"
         />
       </div>
-    </div>
-  </Col>
-</Row>
+            </div>
+          </Col>
+        </Row>
         {/* ----------------------- */}
-<Row>
-<Col>
-        
-        </Col>
-</Row>
-       
-      </div>
+        <Container className="about_space">
+          <Row className="about_us">
+            <Col lg={5}>
+              <div className="about_box">
+                <div className="icon-text-box">
+                  <span className="diamond-box">
+                    <FontAwesomeIcon icon={faTags} className="icon-con" />
+                  </span>
+                  <h5>Affordable Prices</h5>
+                </div>
+                <p>
+                  All our clothes are provided at great prices with regular
+                  discounts.
+                </p>
 
+                <div className="icon-text-box">
+                  <span className="diamond-box">
+                    <FontAwesomeIcon icon={faCrown} className="icon-con" />
+                  </span>
+                  <h5>Individual Approach</h5>
+                </div>
+                <p>
+                  Oversize is known for its unique approach to designing clothes
+                  for its customers.
+                </p>
+
+                <div className="icon-text-box">
+                  <span className="diamond-box">
+                    <FontAwesomeIcon
+                      icon={faShoppingBag}
+                      className="icon-con"
+                    />
+                  </span>
+                  <h5>A Wide Product Range</h5>
+                </div>
+                <p>
+                  We provide a wide variety of plus-size underwear and clothes
+                  to our clients
+                </p>
+
+                <img
+                  src="https://img.freepik.com/free-photo/full-length-portrait-smiling-pretty-woman-isolated-white-wall_231208-703.jpg"
+                  alt="model_img"
+                />
+              </div>
+            </Col>
+            <Col lg={7}>
+              <div className="choose_us">
+                <h3>Why do Women Choose Oversize Clothes?</h3>
+                <div className="icon-text-box choose_us ">
+                  <span>
+                    <FontAwesomeIcon icon={faMinus} className="minus-con" />
+                  </span>
+                  <p>
+                    We offer women a variety of benefits besides our clothes and
+                    underwear. Watch this video to know more.
+                  </p>
+                </div>
+                <img
+                  src="https://media.istockphoto.com/id/1439647356/photo/clothing-racks-at-retail-store.jpg?s=612x612&w=0&k=20&c=bf2_Qcd8EvAKGDbTByqKPPs9KklneFriwNPZC_SGL9w="
+                  alt="shopping_img"
+                />
+                <div className="choose_box">
+                  <img src={model_img_6} alt="Model" />
+                  <div className="choose_content">
+                    <h4>
+                      Our main goal is to offer great clothes designed for
+                      various body sizes and types.
+                    </h4>
+                    <div className="name-founder">
+                      <h5>Samantha Johns</h5>
+                      <span>Founder</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        {/* ----------------------------------------------------- */}
+      </div>
       <Footer />
     </>
   );
 };
-
 export default Home;
